80083245bc1a5c53044fb12f23616db007636f32
NickKotadiya <nickkotadiya7751@email.com>
2024-04-18 15:37:28 +0530

    18-04-2024

---
+++ JAVASCRIPT/Projects/Task 1/calculator.js
@@ -0,0 +1,14 @@
+function insertValue(value) {
+    result.value += value;
+}
+function clearResult() {
+    result.value = '';
+}
+
+function deleteResult() {
+    result.value = result.value.slice(0 , -1);
+}
+
+function calculate() {
+    
+}
