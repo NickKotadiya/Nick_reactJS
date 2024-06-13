a6960e4dffeb58f9c7c4d95e0f03c40742a2c214
NickKotadiya <nickkotadiya7751@email.com>
2024-06-13 14:19:27 +0530

    13-06-2024

---
+++ React_Native/Lacture-All/Axios-Data/my-project/src/App.jsx
@@ -0,0 +1,37 @@
+import './App.css'
+import {BrowserRouter,Routes,Route} from  'react-router-dom'
+import HomePage from './Pages/HomePage'
+import Page from './Pages/Page'
+import Shoppage from './Pages/Shoppage'
+import Login from './Pages/Login'
+import Navbar from './Componetes/Navbar'
+import Footer from './Componetes/Footer'
+import Data from './Pages/Data'
+import Axios from './Pages/Axios'
+
+
+
+function App() {
+
+  /* react router dom */
+
+  return (
+    <>
+    <BrowserRouter>
+       <Navbar/>
+         <Routes>
+           <Route path='/' element={<HomePage/>}></Route>
+           <Route path='/Shoppage' element={<Shoppage/>}></Route>
+           <Route path='/Page' element={<Page/>}></Route>
+           <Route path='/Data' element={<Data/>}></Route>
+           <Route path='/Axios' element={<Axios/>}></Route>
+           <Route path='/Login'element={<Login/>}></Route>
+          </Routes>
+        {<Footer/> }
+     </BrowserRouter>
+     
+   </>
+  )
+}
+
+export default App
---
+++ React_Native/Lacture-All/vite-11-06-2024/apolo/src/App.jsx
@@ -0,0 +1,17 @@
+import CakeContainer from './components/CakeContainer';
+import ProductData from './components/ProductData'
+import  './App.css'
+
+
+const App = () => {
+  return (
+    <div>
+      <ProductData/>
+      <CakeContainer/>
+
+      { /**************/}
+    </div>
+  )
+}
+
+export default App
